3aa28e4e0974bddc9c0decd7bfaa86a00f09683a
Bogdan Rotaru <rotarubogdan96@gmail.com>
2019-12-12 14:11:43 +0200

    Pages, routing and navigation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { ArtistsPageComponent } from './pages/artists-page/artists-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
+import {BsDatepickerModule} from 'ngx-bootstrap';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  {path: 'welcome', component: WelcomePageComponent},
+  {path: 'dashboard', component: DashboardPageComponent},
+  {path: 'artists', component: ArtistsPageComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    DashboardPageComponent,
+    ArtistsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    BsDatepickerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
